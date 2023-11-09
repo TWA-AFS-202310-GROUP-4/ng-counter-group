8b9783f2097959922319a159faf280084b244713
Xinwei <xlu15@slb.com>
2023-11-09 16:37:29 +0800

    feat: can show three counters with default number

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+  @ Input() number = 0;
+
+  onIncrease() {
+    this.number++;
+  }
+
+  onDecrease() {
+    this.number--;
+  }
+}

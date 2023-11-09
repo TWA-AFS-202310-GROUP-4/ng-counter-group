b8b7d8e34c314cee54dbbddcd35c0a64b5530f6f
Dexin Wang <DWang36@slb.com>
2023-11-09 16:17:09 +0800

    feat:add the counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConterComponent } from './conter/conter.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConterComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,

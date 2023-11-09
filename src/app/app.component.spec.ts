01868649369c13d69b9d58d95d283704bbdc3231
Diana Jing <DJing@slb.com>
2023-11-09 19:02:02 +0800

    test: add countergroup Component in app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { CounterGroupComponent } from './counter-group/counter-group.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent, CounterComponent]
+    declarations: [AppComponent, CounterComponent, CounterGroupComponent]
   }));
 
   it('should create the app', () => {
